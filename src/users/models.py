59a519d52fecf8bbe303262a21a8147ae2bec4c7
Mina Miljkovic <mina_miljkovic@epam.com>
2025-03-04 13:50:08 +0100

    refactor: update User model with new column names and structure

--- src/users/models.py
+++ src/users/models.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from data_base.database import Base
+from data_base.models import Base
 
 class User(Base):
     __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    login = Column(String, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, index=True)
     hashed_password = Column(String, nullable=False)
-
     projects = relationship("ProjectAccess", back_populates="user")
